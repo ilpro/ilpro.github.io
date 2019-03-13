18c49aa6b556c606ab6c36b16465534de8adb4e7
ilpro <ilpro93@gmail.com>
2019-03-13 12:23:41 +0200

    change style.css

--- public/js/page.login.js
+++
@@ -1,7 +0,0 @@
-$(document).on("keyup", function (e) {
-	if(e.keyCode == 13)
-	  dataSocket.emit('userLogin', JSON.stringify({
-		"email": $(".lg-form-email-input").val(),
-		"pass": $(".lg-form-password-input").val(),
-	  }));
-});
