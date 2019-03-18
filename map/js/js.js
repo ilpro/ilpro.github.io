64c8d1c4bd555138f11776cc1faef07e165c822a
Ilya <ilpro93@gmail.com>
2019-03-18 22:28:53 +0200

    add new folder

---
+++ map/js/js.js
@@ -0,0 +1,15 @@
+$('.arrowLeft').click(function() {
+		$('.population-rating').toggleClass('active');
+		$(this).toggleClass('rat2');
+		// $('.active').animate({left: 0 + 'px'},3000);
+	});
+
+
+
+
+$('.arrowTop').click(function(){
+
+	$('.table-data').toggleClass('activeBottom');
+	$(this).toggleClass('bot2');
+});
+
