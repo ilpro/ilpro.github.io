819e803cbc0ddd289c20567294a55dacc87d6746
ilpro <ilpro93@gmail.com>
2019-03-21 12:01:23 +0200

    change style

--- map/js/js.js
+++ map/js/js.js
@@ -1,17 +1,41 @@
+// $('.arrowLeft').click(function() {
+// 		$('.population-rating').toggleClass('active');
+// 		$(this).toggleClass('rat2');
+// 		// $('.active').animate({left: 0 + 'px'},3000);
+// 	});
+
 $('.arrowLeft').click(function() {
 		$('.population-rating').toggleClass('active');
-		$(this).toggleClass('rat2');
+		$('.arrowLeft2').css('display','block');
+		$(this).css('display','none');
+		// $('.active').animate({left: 0 + 'px'},3000);
+	});
+
+
+$('.arrowLeft2').click(function() {
+	$('.population-rating').toggleClass('active');
+		$('.arrowLeft').css('display','block');
+		$(this).css('display','none');
 		// $('.active').animate({left: 0 + 'px'},3000);
 	});
 
 
 
-
 $('.arrowTop').click(function(){
-
 	$('.table-data').toggleClass('activeBottom');
-	$(this).toggleClass('bot2');
+	$('.arrowTop2').css('display', 'block');
+	$(this).css('display', 'none');
 });
+
+$('.arrowTop2').click(function(){
+	$('.table-data').toggleClass('activeBottom');
+	$('.arrowTop').css('display', 'block');
+	$(this).css('display', 'none');
+});
+
+
+
+
 
 
 
