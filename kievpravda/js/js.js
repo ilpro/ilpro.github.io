6c652c51dc39c31a06b3f3e8f4c4dd1cdbd3196b
Ilya <ilpro93@gmail.com>
2019-04-01 20:43:00 +0200

    change style

---
+++ kievpravda/js/js.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: false,
+    loopFillGroupWithBlank: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  
